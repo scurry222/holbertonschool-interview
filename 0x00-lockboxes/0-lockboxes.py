abe021be5a9e7ca3435c117e8871ce73dbec185f
scurry222 <scurry222@gmail.com>
2020-02-01 15:35:15 -0800

    examples in comments

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,27 +3,34 @@
     Each box is numbered sequentially from 0 to n - 1 and each box may contain
     keys to the other boxes.
     Write a method that determines if all the boxes can be opened.
+
+    Examples:
+        [[1], [2], [3], [4], []] => return True
+
+        [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]] => return True
+
+        [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]] => return False
 """
 
 
 def canUnlockAll(boxes):
-    """ canUnlockAll - return true if all boxes can be opened, false if not
+    """ canUnlockAll - Return true if all boxes can be opened, false if not.
         attrs:
-            boxes (List of lists): boxes with list indicies that 'open' other
-                                   boxes
+            boxes (List of lists): Boxes with list indicies that 'open' other
+                                   boxes.
     """
     new = [False for i in range(len(boxes))]
     return checkBoxes(boxes, new, 0) is True
 
 
 def checkBoxes(boxes, new, index):
-    """ checkBoxes - recursively check path of key
+    """ checkBoxes - Recursively check the path of keys unlocking boxes.
         attrs:
-            boxes (List of lists): boxes with list indicies that 'open' other
-                                   boxes
-                       new (list): list of booleans to 'mark' each box at a
-                                    specific index
-                       index(int): index to a box to check through 'keys'
+            boxes (List of lists): Boxes with list indicies that 'open' other
+                                   boxes.
+                       new (list): List of booleans to 'mark' each box at a
+                                   specific index.
+                       index(int): Index to a box to check through keys.
     """
     if new[index] is True:
         # print("nope! go back...")
